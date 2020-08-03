2fdbace6f0fe621ad446c14815ee8160498169ff
talialina <natalia@nataliamolina.com>
2020-08-03 15:48:03 -0400

    adjusting missin pull

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,58 +5,39 @@ import Badge from 'react-bootstrap/Badge';
 import { useHistory } from 'react-router-dom';
 
 const Nav = () => {
-<<<<<<< HEAD
-  let history = useHistory();
-  const handleClickHome = () => {
-    history.push('/');
-  };
-=======
-    const [search, setSearch] = useState("");
-    const formEl = React.useRef(null);
+  const [search, setSearch] = useState('');
+  const formEl = React.useRef(null);
 
-    const handleSubmitBar = (event) => {
-        event.preventDefault();
-        setSearch(event.target.elements.searchbar.value);
-    
-        event.target.elements.searchbar.value = "";
-    }
->>>>>>> 4360a0d2a4983d20e3879cc98780f988254fc89e
+  const handleSubmitBar = (event) => {
+    event.preventDefault();
+    setSearch(event.target.elements.searchbar.value);
+
+    event.target.elements.searchbar.value = '';
+  };
 
   return (
     //creates eventual searchbar from React-bootstrap
-    <Container>
-<<<<<<< HEAD
+    let history = useHistory();
+    const handleClickHome = () => {
+      history.push('/');
+     };
+   <Container>
       <div className="heading">
         <Badge variant="info" onClick={handleClickHome}>
           Modern Art Generator
         </Badge>
-        <Form>
+        <Form ref={formEl} onSubmit={handleSubmitBar}>
           <Form.Row>
             <Form.Control
               id="searchbar"
               size="200px"
               type="text"
               placeholder="Search your art"
+              defaultValue={search}
             ></Form.Control>
           </Form.Row>
         </Form>
       </div>
-=======
-        <div className="heading">
-        <Badge variant="info" onClick={handleClickHome}>Modern Art Generator</Badge>
-    <Form ref={formEl} onSubmit={handleSubmitBar}>
-        <Form.Row>
-            <Form.Control 
-                id="searchbar" 
-                size="200px" 
-                type ="text" 
-                placeholder="Search your art"
-                defaultValue={search}
-                ></Form.Control>
-        </Form.Row>
-    </Form>
-    </div>
->>>>>>> 4360a0d2a4983d20e3879cc98780f988254fc89e
     </Container>
   );
 };
