5523312d19cffd43ac21618b531fea355b4523fc
Frances Dalla Torre <francesdallatorre@hotmail.com>
2020-08-03 17:16:37 -0400

    changed questionnaire to quiz

--- client/src/components/Questionnaire.js
+++ client/src/components/Questionnaire.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // quesitons will change according to the API theme
-const Questionnaire = () => {
+const Quiz = () => {
   const questions = [
     {
       id: 1,
@@ -39,19 +39,17 @@ const Questionnaire = () => {
   const [question, setQuestion] = useState(questions[0]);
   const [currentIndex, setCurrentIndex] = useState(1);
   const [answers, setAnswers] = useState([]);
-  const [complete, setComplete] = useState("");
 
   const click = (answer) => {
     setAnswers([...answers, answer]);
 
+    let photography = 0;
+    let furniture = 0;
+    let paintings = 0;
+    let weapons = 0;
+    let sculture = 0;
 
-// testing the if else for funtion that determines a path
-    if (answer === "Paris") {console.log("bonjour")}
-    
-
-   
-if (currentIndex >= questions.length) {
-      setComplete("complete");
+    if (currentIndex >= question.length) {
       return;
     }
 
@@ -61,12 +59,8 @@ if (currentIndex >= questions.length) {
     console.log(currentIndex);
   };
 
-
-
   return (
     <div className="questionnaire">
-      {complete && <p>{complete}</p>}
-
       <div id="question">{question?.q}</div>
       {question?.a.map((answer, i) => {
         return (
@@ -74,13 +68,11 @@ if (currentIndex >= questions.length) {
             <button onClick={() => click(answer?.one)}>{answer.one}</button>
             <button onClick={() => click(answer?.two)}>{answer.two}</button>
             <button onClick={() => click(answer?.three)}>{answer.three}</button>
-            
           </div>
-          
         );
       })}
     </div>
   );
 };
 
-export default Questionnaire;
+export default Quiz;
