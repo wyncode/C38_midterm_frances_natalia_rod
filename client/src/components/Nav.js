42ae5355b53d520ab5cd96c639240dffd9476e31
Rodrigo Poma <rodpoma@gmail.com>
2020-08-05 15:01:35 -0600

    comments added for accessibility

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,35 +1,41 @@
 import React, { useState, useContext } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
-function Nav() {
+function Nav() { 
+
+  // The folliwng functions, using the history prop, route you to the respective galleries upon clicking their respective buttons in the dropdown menu
+
   let history = useHistory();
 
-  const handleClickPaintings = () => {
-    history.push('/Paintings');
-  };
-  const handleClickSculpt = () => {
-    history.push('/Sculpture');
-  };
-  const handleClickWeapons = () => {
-    history.push('/Weapons');
-  };
-  const handleClickPhoto = () => {
-    history.push('/Photography');
-  };
+    const handleClickPaintings = () => {
+      history.push('/Paintings');
+    };
+    const handleClickSculpt = () => {
+      history.push('/Sculpture');
+    };
+    const handleClickWeapons = () => {
+      history.push('/Weapons');
+    };
+    const handleClickPhoto = () => {
+      history.push('/Photography');
+    };
 
   return (
     <nav className="arNav">
+
+      {/* link element is an easy way to route back to the home page */}
+
       <Link to="/" className="ARLOGO"></Link>
-      <DropdownButton id="dropdown-item-button" title="Galleries">
-        <Dropdown.ItemText>Navigate:</Dropdown.ItemText>
+
+      {/* The following dropdown menu code allows for multiple different route activated by onClicking, with the functions above as arguements for each respective route */}
+              {/* variant for dropdown button gives it a color */}
+
+      <DropdownButton id="dropdown-item-button" variant="secondary" title="Galleries">
         <Dropdown.Item onClick={handleClickPaintings}>Paintings</Dropdown.Item>
         <Dropdown.Item onClick={handleClickSculpt}>Sculpture</Dropdown.Item>
-        <Dropdown.Item onClick={handleClickWeapons}>
-          Weapons and Ammunition
-        </Dropdown.Item>
+        <Dropdown.Item onClick={handleClickWeapons}>Weapons and Ammunition</Dropdown.Item>
         <Dropdown.Item onClick={handleClickPhoto}>Photographs</Dropdown.Item>
       </DropdownButton>
     </nav>
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config();                                                           //necessary to interpolate API key
 }
 
 const express = require('express');
@@ -8,56 +8,60 @@ const app = express();
 const axios = require('axios');
 const { response } = require('express');
 
+// ${process.env.HAM_API_KEY} interpolates the API key from the .env 
 //calling out harvard API- PAINTINGS
-app.get('/api/paintings', async (request, response) => {
-  const {page} = request.query
+
+app.get('/api/paintings', async (request, response) => {                                //'api/paintings' becomes the route used to fetch data on the front end
+  const {page} = request.query                                                          //request.query allows you to manipulate the pages within the Gallery's component page
   try {
-  const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Paintings&century=20th+century&page=${page}`)
-  response.json(data);
+    const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Paintings&century=20th+century&page=${page}`)
+    response.json(data);
   } catch (e){
-  console.log(e);
-  response.status(500).send({ error:e.message });
+    console.log(e);
+    response.status(500).send({ error:e.message });
   }
 });
 
-//calling out harvard API- SCULPTURE
-app.get('/api/sculpture', async (request, response) => {
-  const {page} = request.query
+// calling out harvard API- SCULPTURE
+
+app.get('/api/sculpture', async (request, response) => {                                 //'api/sculpture' becomes the route used to fetch data on the front end
+  const {page} = request.query                                                           //request.query allows you to manipulate the pages within the Gallery's component page
   try {
-  const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Sculpture&century=20th+century&page=${page}`)
-  response.json(data);
+    const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Sculpture&century=20th+century&page=${page}`)
+    response.json(data);
   } catch (e){
-  console.log(e);
-  response.status(500).send({ error:e.message });
+    console.log(e);
+    response.status(500).send({ error:e.message });
   }
 });
 
 //calling out harvard API- WEAPONS AND AMMO
-app.get('/api/weapons', async (request, response) => {
-  const {page} = request.query
+
+app.get('/api/weapons', async (request, response) => {                                    //'api/weapons' becomes the route used to fetch data on the front end
+  const {page} = request.query                                                            //request.query allows you to manipulate the pages within the Gallery's component page
   try {
-  const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Weapons+and+Ammunition&page=${page}`)
-  response.json(data);
+    const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Weapons+and+Ammunition&page=${page}`)
+    response.json(data);
   } catch (e){
-  console.log(e);
-  response.status(500).send({ error:e.message });
+    console.log(e);
+    response.status(500).send({ error:e.message });
   }
 });
 
 //calling out harvard API- PHOTOGRAPHY
-app.get('/api/photography', async (request, response) => {
-  const {page} = request.query
+
+app.get('/api/photography', async (request, response) => {                                //'api/photography' becomes the route used to fetch data on the front end
+  const {page} = request.query                                                            //request.query allows you to manipulate the pages within the Gallery's component page
   try {
-  const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Photographs&period=Modern&page=${page}`)
-  response.json(data);
+    const { data } = await axios.get(`https://api.harvardartmuseums.org/object?apikey=${process.env.HAM_API_KEY}&classification=Photographs&period=Modern&page=${page}`)
+    response.json(data);
   } catch (e){
-  console.log(e);
-  response.status(500).send({ error:e.message });
+    console.log(e);
+    response.status(500).send({ error:e.message });
   }
 });
 
-
-// END DEMO
+//End of API requests
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
